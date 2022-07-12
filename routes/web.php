295d17f07125600bc89f3e25006a7d8aaff9865f
jessica-camila <jessica-camila@sicredi.com.br>
2022-07-12 14:04:57 -0300

    12.07 primeiro contato

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SeriesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::{verbo http}('{sua rota}', {Código a ser executado}); */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/series', [SeriesController::class,'listarSeries']);
